1ddd936a2fd865890801d5cf685d512a0fcae002
huylgia <lghuyvn2000@gmail.com>
2023-06-30 08:27:40 +0000

    add: build logger to log error

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 import uvicorn
 from api import main_router 
+from utils import build_logger
+
+LOGGER = build_logger("logger", "error")
 
 app = FastAPI()
 app.include_router(main_router(phase_id=1, prob_ids=[1,2]))
 app.include_router(main_router(phase_id=2, prob_ids=[1,2]))
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+	exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+	LOGGER.info(f"{request}: {exc_str}")
+	content = {'status_code': 10422, 'message': exc_str, 'data': None}
+	return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 if __name__ == "__main__":
     uvicorn.run(
--- utils.py
+++ utils.py
@@ -1,7 +1,44 @@
 import timeit
 from typing import Callable, Mapping, Any
 
+import logging
+import datetime
+import os
+
+import pathlib
+__dir__ = pathlib.Path(__file__).parent
+
+class MyFormatter(logging.Formatter):
+    converter=datetime.datetime.fromtimestamp
+    def formatTime(self, record, datefmt=None):
+        ct = self.converter(record.created)
+        if datefmt:
+            s = ct.strftime(datefmt)
+        else:
+            t = ct.strftime("%Y-%m-%d %H:%M:%S")
+            s = "%s,%03d" % (t, record.msecs)
+        return s
+
 def measure_execute_time(func: Callable, args: Mapping[str, Any], loop: int):
     result = timeit.timeit(lambda: func(**args), number=loop)
 
     return result/loop
+
+def build_logger(log_dir, log_name):
+    log_dir = (__dir__/log_dir).__str__()
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    # set handler
+    handler = logging.FileHandler(f"{log_dir}/{log_name}.log", encoding="utf-8")
+    formatter = MyFormatter("%(asctime)s %(name)s %(message)s", datefmt='%d-%m-%Y %H:%M:%S.%f')
+    handler.setFormatter(formatter)
+
+    # create logger
+    logger = logging.getLogger(log_name)
+    logger.propagate = False
+    
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+
+    return logger
